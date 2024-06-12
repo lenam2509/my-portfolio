5104e6ee6a09be7a91e60fc9adfeb9be1178642e
Le Viet Nam <levietnam2509@gmail.com>
2024-06-12 17:56:32 +0700

    chore: Update tailwind configuration and CSS variables

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,10 +1,35 @@
+"use client";
+
 import Link from "next/link";
 import { FaPhoneAlt, FaFacebook, FaDiscord } from "react-icons/fa";
 import { SiZalo } from "react-icons/si";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
+import { useState } from "react";
 
 export default function Navbar() {
+  const { scrollY } = useScroll();
+  const [hiden, setHiden] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious() ?? 0;
+    if (latest > previous && latest > 50) {
+      setHiden(true);
+    } else {
+      setHiden(false);
+    }
+  });
+
   return (
-    <header className="w-full shadow-md bg-white p-2">
+    <motion.header
+      variants={{
+        hidden: { y: "-100%" },
+        visible: { y: 0 },
+      }}
+      animate={hiden ? "hidden" : "visible"}
+      initial="visible"
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+      className=" sticky z-50 top-0 w-full shadow-md bg-white p-2"
+    >
       <nav className="flex items-center justify-between mx-auto max-w-[1400px]">
         <Link className="text-md sm:text-2xl  font-bold text-blue-500" href="/">
           LE VIET NAM
@@ -19,6 +44,6 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
-    </header>
+    </motion.header>
   );
 }
